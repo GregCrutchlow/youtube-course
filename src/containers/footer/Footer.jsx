2dd9694e8ac75bca9b45a086e325e2238ca9e3cb
GregCrutchlow <gregcrutchlow@gmail.com>
2023-03-27 10:43:46 -0400

    added media queries for mobile for footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -19,9 +19,12 @@ const Footer = () => {
 							alt='GPT3 Logo'
 						/>
 					</a>
-					<p>Crechterwoord K12 182 DK Alkjkcb, <br /> All Rights Reserved</p>
+					<p>
+						Crechterwoord K12 182 DK Alkjkcb, <br /> All Rights Reserved
+					</p>
 				</div>
 				<div className='gpt3__footer-link_div'>
+					<div className='gpt3__footer-link_div-bar'></div>
 					<h4>Links</h4>
 					<p>Overons</p>
 					<p>Social Media</p>
@@ -29,21 +32,23 @@ const Footer = () => {
 					<p>Contact</p>
 				</div>
 				<div className='gpt3__footer-link_div'>
+					<div className='gpt3__footer-link_div-bar'></div>
 					<h4>Company</h4>
 					<p>Terms & Conditions</p>
 					<p>Privacy Policy</p>
 					<p>Contact</p>
 				</div>
 				<div className='gpt3__footer-link_div'>
+					<div className='gpt3__footer-link_div-bar'></div>
 					<h4>Get in Touch</h4>
 					<p>Crechterwoord K12 182 DK Alkjkcb</p>
 					<p>085-132567</p>
 					<p>info@payme.net</p>
 				</div>
-            </div>
-            <div className="gpt3__footer-copyright">
-                <p>© 2021 GPT-3, All Rights Reserved</p>
-            </div>
+			</div>
+			<div className='gpt3__footer-copyright'>
+				<p>© 2021 GPT-3, All Rights Reserved</p>
+			</div>
 		</div>
 	);
 }
